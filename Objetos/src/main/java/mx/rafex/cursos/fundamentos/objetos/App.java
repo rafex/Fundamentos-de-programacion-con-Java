c7452bedb1dbe29f56a6f25832ca9083acf184be
Luis Enrique <luisangeltkd@hotmail.com>
2019-05-03 11:30:52 -0500

    a ver que pasa

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        int i = 49;
+
+        while (true) {  
+
+            i++;
+
+            System.out.println ("incrementa a : " + i);
+
+            if (i==100) { break;}
+
+        }                
+
+
     }
 }
