8592fc62a6a3f763edb0d4b10c368cde10493534
abel3105 <49993461+abel3105@users.noreply.github.com>
2019-05-03 11:26:05 -0500

    crear
    while que imprime  del 1 al 80

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        int a=1;
+        while (a<=80) {
+        	System.out.println(a);
+        	a ++;	
+        }
     }
 }
