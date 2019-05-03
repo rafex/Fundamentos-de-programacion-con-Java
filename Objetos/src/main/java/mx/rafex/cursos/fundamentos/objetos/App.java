654912b87331d8ddaf70e483ba2e421c901228cd
carloshernandezfabian <carlos_hdz.fabian@yahoo.com.mx>
2019-05-03 12:21:29 -0500

    Update App.java

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -4,18 +4,13 @@ package mx.rafex.cursos.fundamentos.objetos;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        
-        for(int i = 0; i <= 10; i ++) {
-        	
-        System.out.println("Los numeros son:" + i );
-        
-    }
-   }
-}
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 
+		for (int i = 0; i <= 10; i++) {
+
+			System.out.println( + i);
+		}
+	}
+}
