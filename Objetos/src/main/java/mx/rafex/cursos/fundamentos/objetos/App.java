b171cbc840e766bbf679a97d1a7f399c08de70b1
AndresSanG <49949220+AndresSanG@users.noreply.github.com>
2019-05-03 11:29:44 -0500

    Update App.java
    while del 1 al 1000

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+        
+     
+        int i = 1; 
+    	System.out.println(i);	   
+    
+       while (i<1000){
+    	    i = i + 1;
+       }   	}
 }
