4909e3a2d8d19311eb05eabd7da8ae707115cf33
galb23 <galb23@gmail.com>
2019-05-03 11:28:05 -0500

    do while
    imprime del 100 al 200

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int numero=100;
         System.out.println( "Hello World!" );
+        
+        do {
+        	System.out.println("los numeros son"+numero);
+        numero=numero+1;}
+        while(numero<200);
+        	
     }
 }
