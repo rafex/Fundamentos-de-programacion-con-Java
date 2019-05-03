bb2080eb55a3424f4d0825ced26609449afeeb55
SergioRmz <49949367+SergioRmz@users.noreply.github.com>
2019-05-03 11:29:24 -0500

    Update App.java

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        for(Integer i = 300; i <= 400; i++) {
+        	System.out.println(i);
+        }
     }
 }
