c459f6aed133e5e8e7fd42c618f76f4bd492cfe7
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-03 11:30:05 -0500

    Update App.java

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+       int x = 1;
+       
+       for (int i=1; i<=100; i=i+1) {
+       
+       x = x+1;
+       }
+       
     }
 }
