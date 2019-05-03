4654c20f513c6a5e13a749f567f9221d5fe7c3d0
carloshernandezfabian <carlos_hdz.fabian@yahoo.com.mx>
2019-05-03 11:45:25 -0500

    ciclo for
    impreme numeros de 1 a 10

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+        for(int i = 0; i <= 10; i ++) {
+        	
+        System.out.println("Los numeros son:" + i );
+        
     }
+   }
 }
+
