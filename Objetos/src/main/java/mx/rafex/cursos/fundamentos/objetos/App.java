709a58a0f3f0218aeda46a37a8263c1f490be84c
Mauricio <mao_monroy@hotmail.com>
2019-05-03 11:34:24 -0500

    Update App.java
    Un while que muestre los numero de 50 a 80.

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        //Escribir un while que imprima del 50 al 80
+        int num = 50;
+    	System.out.println("Los numeros son ");
+        while ( num <80) {
+        	num = num + 1;
+        	System.out.println(num);
+        }        
     }
 }
