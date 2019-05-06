a17437ac9c5d67c7eb7b59bb6ed3470071107f0a
abel3105 <49993461+abel3105@users.noreply.github.com>
2019-05-06 11:38:57 -0500

    abel 1 al 100
    do and while

--- Objetos/src/main/java/App.java
+++ Objetos/src/main/java/App.java
@@ -5,6 +5,14 @@ public class App {
 
         System.out.println("Hello World !!!");
 
+        // abel
+        int numero = 1;
+
+        do {
+            System.out.println("los numeros son" + numero);
+            numero = numero + 1;
+        } while (numero < 100);
+
     }
 
 }
