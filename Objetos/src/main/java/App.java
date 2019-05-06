ff43900d52523e5b6eeb26bc94c4d2691eb1c8e1
IMANOL <DiegoSVertti@gmail.com>
2019-05-06 11:41:46 -0500

    while
    ejercicio de comprobacion

--- Objetos/src/main/java/App.java
+++ Objetos/src/main/java/App.java
@@ -5,6 +5,11 @@ public class App {
 
         System.out.println("Hello World !!!");
 
+        int l = 1; // este simple while es propiedad De Imanol-Master
+        while (l < 100) {
+            l = l + 1;
+            System.out.println(l);
+        }
     }
 
 }
