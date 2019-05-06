00433897833eedd820bdad7b9b141ff6ef0481ca
Luis Enrique <luisangeltkd@hotmail.com>
2019-05-06 11:41:03 -0500

    A ver que pasa, espero no ser el último.

--- Objetos/src/main/java/App.java
+++ Objetos/src/main/java/App.java
@@ -5,6 +5,14 @@ public class App {
 
         System.out.println("Hello World !!!");
 
+        // Luis Enrique
+        final int numeroLuis = 10;
+
+        if (numeroLuis == 10) {
+
+        }
+        System.out.println("El numero es: " + numeroLuis);
+
     }
 
 }
