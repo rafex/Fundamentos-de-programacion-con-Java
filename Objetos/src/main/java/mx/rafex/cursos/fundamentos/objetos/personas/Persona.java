71d8d4cb0042b5a5aa6d383ca2f50d3714dde4c8
Raúl González <rafex.dev@gmail.com>
2019-05-06 10:48:47 -0500

    inicio de objetos e interfaz para usuarlo

---
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/personas/Persona.java
@@ -0,0 +1,33 @@
+package mx.rafex.cursos.fundamentos.objetos.personas;
+
+public class Persona {
+
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+}
