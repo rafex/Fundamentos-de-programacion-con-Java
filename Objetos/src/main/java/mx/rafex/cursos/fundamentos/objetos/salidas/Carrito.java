1b1eb99a495cf10051fa583206a8d57b3671e719
AndresSanG <49949220+AndresSanG@users.noreply.github.com>
2019-05-06 13:19:07 -0500

    Update Carrito.java
    cambios con import de <producto> y geters y seters

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/salidas/Carrito.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/salidas/Carrito.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import mx.rafex.cursos.fundamentos.objetos.productos.Producto;
+
 public class Carrito implements Serializable {
 
     /**
@@ -20,4 +22,60 @@ public class Carrito implements Serializable {
     private BigDecimal descuento;
     private Long id;
     private LocalDate fecha;
+
+    public ArrayList<Producto> getListaDeProductos() {
+        return this.listaDeProductos;
+    }
+
+    public void setListaDeProductos(final ArrayList<Producto> listaDeProductos) {
+        this.listaDeProductos = listaDeProductos;
+    }
+
+    public BigDecimal getPrecioSinIva() {
+        return this.precioSinIva;
+    }
+
+    public void setPrecioSinIva(final BigDecimal precioSinIva) {
+        this.precioSinIva = precioSinIva;
+    }
+
+    public BigDecimal getPrecioConIva() {
+        return this.precioConIva;
+    }
+
+    public void setPrecioConIva(final BigDecimal precioConIva) {
+        this.precioConIva = precioConIva;
+    }
+
+    public BigDecimal getCostoEnvio() {
+        return this.costoEnvio;
+    }
+
+    public void setCostoEnvio(final BigDecimal costoEnvio) {
+        this.costoEnvio = costoEnvio;
+    }
+
+    public BigDecimal getDescuento() {
+        return this.descuento;
+    }
+
+    public void setDescuento(final BigDecimal descuento) {
+        this.descuento = descuento;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(final LocalDate fecha) {
+        this.fecha = fecha;
+    }
 }
