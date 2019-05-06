9efa99c76ac364f6ef75608e42fe5e1d4ba36ae9
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-06 12:32:44 -0500

    se declararon variables y se agregaron getters y setters

--- Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/productos/Productos.java
+++ Objetos/src/main/java/mx/rafex/cursos/fundamentos/objetos/productos/Productos.java
@@ -1,5 +1,32 @@
 package mx.rafex.cursos.fundamentos.objetos.productos;
 
 public class Productos {
+    private Double precio;
+    private String tipo;
+    private String departamento;
+
+    public Double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(final Double precio) {
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(final String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDepartamento() {
+        return this.departamento;
+    }
+
+    public void setDepartamento(final String departamento) {
+        this.departamento = departamento;
+    }
 
 }
