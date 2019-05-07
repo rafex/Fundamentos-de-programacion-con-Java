4cad225461f0f359458363306820bd1a3d72a6cf
Raúl González <rafex@rafex.mx>
2019-05-06 19:15:29 -0500

    creando jar fat

--- App/src/main/java/mx/rafex/cursos/fundamentos/app/App.java
+++ App/src/main/java/mx/rafex/cursos/fundamentos/app/App.java
@@ -4,10 +4,10 @@ package mx.rafex.cursos.fundamentos.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
+
 }
