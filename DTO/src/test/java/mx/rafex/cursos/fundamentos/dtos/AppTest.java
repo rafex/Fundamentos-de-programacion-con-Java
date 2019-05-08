8f523190a9f2ee4764ba0c598cb7a844feb60818
Raúl González <rafex.dev@gmail.com>
2019-05-08 09:24:28 -0500

    separando curso con proyecto

--- DTO/src/test/java/mx/rafex/cursos/fundamentos/dtos/AppTest.java
+++
@@ -1,4 +0,0 @@
-package mx.rafex.cursos.fundamentos.dtos;
-
-public class AppTest {
-}
---
+++ Excepciones/src/main/java/mx/rafex/cursos/fundamentos/excepciones/App.java
@@ -0,0 +1,13 @@
+package mx.rafex.cursos.fundamentos.excepciones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
